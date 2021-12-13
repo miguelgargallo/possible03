031bb646ac88f53e21d0be8bb974cdecd2bc35df
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2021-12-13 10:54:09 +0200

    test refactoring

--- db/structure.sql
+++ db/structure.sql
@@ -827,7 +827,8 @@ CREATE TABLE public.dnskeys (
     updator_str character varying,
     legacy_domain_id integer,
     updated_at timestamp without time zone,
-    validation_datetime timestamp without time zone
+    validation_datetime timestamp without time zone,
+    failed_validation_reason character varying
 );
 
 
@@ -1195,6 +1196,7 @@ CREATE TABLE public.invoices (
     buyer_vat_no character varying,
     issue_date date NOT NULL,
     e_invoice_sent_at timestamp without time zone,
+    payment_link character varying,
     CONSTRAINT invoices_due_date_is_not_before_issue_date CHECK ((due_date >= issue_date))
 );
 
@@ -5401,6 +5403,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220106123143'),
 ('20220113201642'),
 ('20220113220809'),
-('20220316140727');
-
-
+('20220316140727'),
+('20220124105717'),
+('20220216113112'),
+('20220228093211');
